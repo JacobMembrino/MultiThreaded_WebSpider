f887b8ed61d280cfaa2880dabfd20130707abbe6
JacobMembrino <jacob.membrino@gmail.com>
2023-05-15 17:29:23 +0000

    Fixed pymysql issues

--- Web_Spider.py
+++ Web_Spider.py
@@ -1,45 +1,64 @@
-# first, run installs on the following software:
-# $ pip install beautifulsoup4
-
-
+import argparse
 import urllib.request
-import re, sys, mechanize
-import pymysql.cursors
+import re
+import pymysql
 from bs4 import BeautifulSoup
+import threading
 
-website = sys.argv[1]
-depth = sys.argv[2]
 
-request = urllib.request.urlopen(website)
-my_HTML = request.read().decode("utf8")
-print(my_HTML)
+def scrape_website(url, depth):
+    request = urllib.request.urlopen(url)
+    my_HTML = request.read().decode("utf8")
 
-soup = BeautifulSoup(request, 'html.parser')
+    soup = BeautifulSoup(my_HTML, 'html.parser')
+    forms = soup.find_all("div")
 
-forms = soup.find_all("div")
-
-if depth == 0 :
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passwd="root",
+    if depth == 0:
+        mydb = pymysql.connect(
+            host="localhost",
+            user="root",
+            password="root",
             database="mydatabase")
-else if depth >= 1 :
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="usr",
-        passwd="password",
+    elif depth >= 1:
+        mydb = pymysql.connect(
+            host="localhost",
+            user="usr",
+            password="password",
             database="mydatabase")
-else :
-    print("Enter Valid Depth Int.")
+    else:
+        print("Enter Valid Depth Int.")
+        return
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-for column in forms:
-    form = column.find('td').text.strip()
-    print(form,'\n')
+    for column in forms:
+        form = column.find('td').text.strip()
+        print(form, '\n')
 
-    query = "INSERT INTO ROW1 (form) VALUES (%s)"
-    arguments = (form)
-    mycursor.execute(query, arguments)
-    mydb.commit()
+        query = "INSERT INTO ROW1 (form) VALUES (%s)"
+        arguments = (form,)
+        mycursor.execute(query, arguments)
+        mydb.commit()
+
+    mydb.close()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Multi-threaded web spider with variable depth.')
+    parser.add_argument('website', help='Website URL to scrape')
+    parser.add_argument('depth', type=int, help='Depth of the web spider')
+    args = parser.parse_args()
+
+    website = args.website
+    depth = args.depth
+
+    # Start multi-threaded web spider
+    spider_thread = threading.Thread(
+        target=scrape_website, args=(website, depth))
+    spider_thread.start()
+    spider_thread.join()
+
+
+if __name__ == "__main__":
+    main()
